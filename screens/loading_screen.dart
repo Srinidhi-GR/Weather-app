84861082dd004ffe005af54f19068730695983ad
Srinidhi-GR <168341432+Srinidhi-GR@users.noreply.github.com>
2024-06-17 16:51:25 +0530

    Add files via upload

---
+++ screens/loading_screen.dart
@@ -0,0 +1,29 @@
+import 'package:clima/services/location.dart';
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  LoadingScreenState createState() => LoadingScreenState();
+}
+
+class LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
